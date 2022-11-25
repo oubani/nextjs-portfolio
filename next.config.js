153cb4983e6157156ffb41ceca68181e8690ae90
oubani <oubaniayoub@gmail.com>
2022-11-25 12:00:47 +0100

    upgrade to next js 12

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-	reactStrictMode: true,
-	images: {
-		loader: 'imgix',
-		path: 'https://stupefied-austin-962504.netlify.app/',
-	},
+  reactStrictMode: true,
 };
